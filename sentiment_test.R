dabd7ff3dad7be1029f4d5456c84bc2c4a55d8a5
Joe Hoskisson <joe.hoskisson@mx.com>
2022-05-16 14:18:02 -0400

    first cut at aggrating by day

--- sentiment_test.R
+++ sentiment_test.R
@@ -7,8 +7,8 @@ library(tidytext)
 library(glue)
 library(stringr)
 
-tweet_file <- read.csv("/Users/joehoskisson/rprojects/social_media_censor_project/test_tweets.csv")
-tweet_texts <- tweet_file$tweet_text
+tweet_file <- read.csv("/Users/joehoskisson/rprojects/social_media_censor_project/egypt_tweets.csv")
+tweet_texts <- tweet_file$translated
 
 ensure_two_sentiment_categories <- function(count_data) {
   df_negative <- data.frame(sentiment=c("negative"), n=c(0))
@@ -40,3 +40,30 @@ for(tweet_text in tweet_texts) {
   print(generate_sentiment_score(tweet_text))
 }
 
+
+# create data frame with day column and score column
+egypt_days <- data.frame(matrix(ncol = 2, nrow = 0))
+names(egypt_days) <- c("date", "sentiment_score")
+
+# iterate over tweets
+for(i in 1:nrow(tweet_file)) {
+  tweet_date <- tweet_file[i,2]
+  tweet_content <- tweet_file[i,4]
+  # check date
+  date_exists <- any(egypt_days$post_date == tweet_date)
+
+  # add row with date when date is not in data
+  clean_date <- substr(tweet_date, 1, 10)
+  if(!date_exists) {
+    egypt_days[nrow(egypt_days) + 1,] <-  c(clean_date, 0)
+  }
+  # calculate sentiment score for tweet text
+  current_sentiment_score <- generate_sentiment_score(tweet_content)
+  # add score to existing score for that date
+  date_row <- which(egypt_days$date == clean_date)
+  previous_sentiment_score <- egypt_days$sentiment_score[date_row]
+  previous_sentiment_score <- as.numeric(previous_sentiment_score)
+  egypt_days$sentiment_score[date_row] <- current_sentiment_score + previous_sentiment_score
+}
+
+write.csv(egypt_days,"/Users/joehoskisson/rprojects/social_media_censor_project/output.csv", row.names = FALSE)
