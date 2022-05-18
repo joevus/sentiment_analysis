f613adbbb7ca89063df89d7bc7e649e56b639354
Joe Hoskisson <joe.hoskisson@mx.com>
2022-05-17 22:24:36 -0400

    refined processing

--- process_sentiment.R
+++ process_sentiment.R
@@ -1,9 +1,10 @@
 # This script takes the project data with sentiment scores per tweet and does the following:
 # - weights each score by like and retweet; each like and retweet adds the orignial score again to the tweet
 # - groups tweets by incident index and before or after marker and gives a total score for each grouping
-# - outputs two csv files
+# - outputs three csv files
 # - one file with all the tweets and weighted sentiment
-# - second file with one row for each incident-time-period grouping (e.g., 1-before, 1-after, 2-before, 2-after,...)
+# - second file with one row for each incident-time-period grouping (e.g., 1-before, 1-after, 2-before, 2-after,...) and the total sentiment score for each group
+# - third file with same incident-time-period grouping but with weighted sentiment totals for each group
 
 library(tidyverse)
 
@@ -20,5 +21,14 @@ for(row in 1:nrow(tweet_file)) {
 
 write_csv(tweet_file, "/Users/joehoskisson/rprojects/social_media_censor_project/all_tweets_with_weighted_sentiment.csv")
 
-# row 5289 creates a problem. All after it are NAs
-# that row messes up in the first sentiment score analysis
+sentiment_sums <- tweet_file %>%
+  group_by(incident_index, type) %>%
+  summarise(total_sentiment=sum(sentiment_score))
+
+write_csv(sentiment_sums, "~/rprojects/social_media_censor_project/incident_sentiment_totals.csv")
+
+weighted_sentiment_sums <- tweet_file %>%
+  group_by(incident_index, type) %>%
+  summarise(total_weighted_sentiment=sum(weighted_score))
+
+write_csv(weighted_sentiment_sums, "~/rprojects/social_media_censor_project/incident_weighted_sentiment_totals.csv")
